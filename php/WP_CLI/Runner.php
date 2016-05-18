wp-cli/wp-cli

commit 7a1499e0a3683f9d51dc18d73d8b087685a9e318

set default global config path;
also, ignore if config file doesn't exist

see #698

php/WP_CLI/Runner.php
@@ -37,6 +37,22 @@ class Runner {
 		}
 	}
 
+	private static function get_global_config_path() {
+		$config_path = getenv( 'WP_CLI_CONFIG_PATH' );
+		if ( isset( $runtime_config['config'] ) ) {
+			$config_path = $runtime_config['config'];
+		}
+
+		if ( !$config_path ) {
+			$config_path = getenv( 'HOME' ) . '/.config/wp-cli.yml';
+		}
+
+		if ( !is_readable( $config_path ) )
+			return false;
+
+		return $config_path;
+	}
+
 	private static function get_project_config_path() {
 		$config_files = array(
 			'wp-cli.local.yml',
@@ -364,11 +380,7 @@ class Runner {
 		list( $this->arguments, $this->assoc_args ) = self::back_compat_conversions(
 			$args, $assoc_args );
 
-		$this->global_config_path = getenv( 'WP_CLI_CONFIG_PATH' );
-		if ( isset( $runtime_config['config'] ) ) {
-			$this->global_config_path = $runtime_config['config'];
-		}
-
+		$this->global_config_path = self::get_global_config_path();
 		$this->project_config_path = self::get_project_config_path();
 
 		$configurator = \WP_CLI::get_configurator();

Inspired715/Wordpress-Calypso

commit 39504a34c765e6551b0dad12ca0a81e502d538ba

Remove `--config=<config>` as a runtime argument
It's been broken since 7a1499e0a3683f9d51dc18d73d8b087685a9e318
(v0.13.0)

php/WP_CLI/Runner.php
@@ -68,10 +68,7 @@ class Runner {
 	 */
 	private function get_global_config_path() {
 
-		if ( isset( $runtime_config['config'] ) ) {
-			$config_path = $runtime_config['config'];
-			$this->_global_config_path_debug = 'Using global config from config runtime arg: ' . $config_path;
-		} else if ( getenv( 'WP_CLI_CONFIG_PATH' ) ) {
+		if ( getenv( 'WP_CLI_CONFIG_PATH' ) ) {
 			$config_path = getenv( 'WP_CLI_CONFIG_PATH' );
 			$this->_global_config_path_debug = 'Using global config from WP_CLI_CONFIG_PATH env var: ' . $config_path;
 		} else {

php/config-spec.php
@@ -17,11 +17,6 @@ return array(
 		'runtime' => '=<url>',
 	),
 
-	'config' => array(
-		'deprecated' => 'Use the WP_CLI_CONFIG_PATH environment variable instead.',
-		'runtime' => '=<path>',
-	),
-
 	'user' => array(
 		'runtime' => '=<id|login|email>',
 		'file' => '<id|login|email>',